0a418820ade59366aba4fd9e5956fc5780056f1a
Bernardo Vale <bernardosilveiravale@gmail.com>
2015-06-01 23:35:12 -0300

    Json de novos erros
    criação do json de erros. Esse arquivo servira de banco de erros do
    script.

--- check_alert.py
+++ check_alert.py
@@ -9,6 +9,9 @@
 #       Versão: v1.0a
 #       LB2 Consultoria - Leading Business 2 the Next Level!
 #---------------------------------------------------------------
+import datetime
+import json
+import time
 from monitoramento_utils import Utils
 
 
@@ -37,35 +40,64 @@ class Monitoring:
         self.critical_count = 0
         self.error_list = []
 
+    def error_is_cleared(self,error_date):
+        """
+        Verifica se o erro ja pode ser limpo
+        :param error_date: Datetime
+        :return: bool
+        """
+        clear_time = datetime.timedelta(minutes=self.clear_time)
+        current_time = datetime.datetime.now()
+        dt = datetime.datetime.strptime(error_date, "%Y%m%d%H%M")
+        clear = dt + clear_time
+        return True if clear < current_time else False
+
     def populate_errors(self):
-        if Utils.file_exists('alertlog_errors.tmp'):
-            with open('alertlog_errors.tmp', 'a') as f:
-                try:
-                    f.seek(0)
-                    file_content = f.readline()
-                    self.last_position = int(file_content)
-                except ValueError:
-                    print "UNKNOWN - Impossivel ler logfile."
-                    exit(3)
-        else:
-            with open('check_alertlog.tmp', 'a') as f:
-                try:
-                    f.write('0')
-                    self.last_position = 0
-                except:
-                    print "UNKNOWN - Erro ao escrever check_alertlog.tmp. Verifique as permissoes."
-                    exit(3)
+        error_list = Utils.read_json(Utils.fullpath('alertlog_errors.json'))['errors']
+        for error in error_list:
+            if self.error_is_cleared(error['date']):
+                print "Clear: %s " % error['message']
+
+    def write_error_json(self):
+        """
+        Escreve no disco o novo json de erros
+        :return:
+        """
+        with open(Utils.fullpath('alertlog_errors.json'), 'w') as f:
+            try:
+                json.dump(self.error_list, f)
+            except:
+                print "UNKNOWN - Impossivel gravar JSON."
+                exit(3)
+
+    def append_error(self, error, is_critical):
+        """
+        Adiciona um novo erro a lista de erros
+        :param error: Mensagem de erro
+        :param is_critical: bool se é critico
+        :return:
+        """
+        current_time = datetime.datetime.now().strftime("%Y%m%d%H%M")
+        new_error = {'message' : error,
+                     'date' : current_time,
+                     'critical' : is_critical
+        }
+        self.error_list.append(new_error)
 
     def find_errors(self, line):
         for critical in self.issues.critical:
             if critical in line:
-                self.error_list.append(line.replace('\n',''))
+                err = line.replace('\n','')
+                #self.error_list.append(err)
                 self.critical_count += 1
+                self.append_error(err, True)
                 return
         for warning in self.issues.warning:
             if warning in line:
-                self.error_list.append(line.replace('\n',''))
+                err = line.replace('\n','')
+                #self.error_list.append(err)
                 self.warning_count += 1
+                self.append_error(err, False)
                 return
 
     def read_partial_file(self):
@@ -129,6 +161,8 @@ def main(logfile, clear_time, config):
     m = Monitoring(logfile, clear_time, config, i)
     m.read_partial_file()
     m.read_log()
+    m.write_error_json()
     #print m.critical_count
     #print m.warning_count
-    print m.error_list
+    #print m.error_list
+    #m.populate_errors()
